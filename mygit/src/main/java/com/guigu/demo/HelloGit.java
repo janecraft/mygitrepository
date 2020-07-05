b69faa0ca3029921cdc1691817a2d283f23f01a4
janecraft1812 <170736623@qq.com>
2020-07-06 00:20:48 +0800

    clone2：janecraft2添加的代码

--- mygit/src/main/java/com/guigu/demo/HelloGit.java
+++ mygit/src/main/java/com/guigu/demo/HelloGit.java
@@ -8,5 +8,6 @@ public class HelloGit {
         System.out.println("主干添加");
         System.out.println("分支添加");
         System.out.println("在线编辑");
+        System.out.println("clone2：janecraft2添加的代码");
     }
 }
